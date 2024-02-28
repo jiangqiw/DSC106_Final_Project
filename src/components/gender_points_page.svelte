0eda633176b4462f51d71959020b93c5a67017f7
Yuhe Tian <yuhetian@Yuhes-MacBook-Air.local>
2024-02-27 18:59:01 -0800

    hehe

--- src/components/gender_points_page.svelte
+++ src/components/gender_points_page.svelte
@@ -5,23 +5,18 @@
     let points = [];
     const totalPoints = 1500;
     const pointsPerSide = Math.ceil(Math.sqrt(totalPoints));
-    const spacing = 12; // Adjust based on the size of the square and SVG area
-
-    let radius = 4; // Example radius
-
+    const spacing = 15; // Adjust based on the size of the square and SVG area
+  
     onMount(() => {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    const pointsPerSideX = Math.floor((width - radius * 2) / spacing);
-    const pointsPerSideY = Math.floor((height - radius * 2) / spacing);
-
-    for (let x = 0; x < pointsPerSideX; x++) {
-        for (let y = 0; y < pointsPerSideY; y++) {
-        points.push({ x: x * spacing + radius, y: y * spacing + radius });
+      for (let x = 0; x < pointsPerSide; x++) {
+        for (let y = 0; y < pointsPerSide; y++) {
+          if (points.length < totalPoints) {
+            points.push({ x: x * spacing, y: y * spacing });
+          }
         }
-    }
+      }
     });
-
+    
     let isVisible = false;
     $: if (index === 4) {
         isVisible = true;
@@ -31,10 +26,10 @@
 </script>
 
 <!-- Use class directive to conditionally apply the 'visible' class based on `isVisible` -->
-<svg class="graph" width="100vw" height="100vh" class:visible={isVisible}>
+<svg class="graph" width="100%" height="100vh" class:visible={isVisible}>
     {#if index === 4}
         {#each points as point}
-        <circle cx={point.x} cy={point.y} r="4" fill="black" />
+        <circle cx={point.x} cy={point.y} r="1" fill="black" />
         {/each}
     {/if}
 </svg>
@@ -43,22 +38,17 @@
     .graph {
         width: 100%;
         height: 100vh;
-        margin: auto;
-        margin-top: 150px;
-        position: center;
+        position: absolute;
+        top: 0;
+        left: 0;
         opacity: 0;
         visibility: hidden;
         transition: opacity 0.5s, visibility 0.5s;
-        justify-content: center;
-        align-items: center;
     }
     .graph.visible {
-        margin: none;
         opacity: 1;
         visibility: visible;
-        padding:0%;
     }
 </style>
-
 
   
